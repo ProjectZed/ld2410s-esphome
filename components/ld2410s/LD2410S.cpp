4e8bf47433a5627dc2083d1c4a46c3042a7262ae
Kenny Tsui <kjtsui15@gmail.com>
2025-03-08 19:37:53 -0500

    refactor: correct data length calculation in LD2410S command acknowledgment

--- components/ld2410s/LD2410S.cpp
+++ components/ld2410s/LD2410S.cpp
@@ -322,7 +322,7 @@ namespace esphome
             pos += sizeof(cmd_ack.command);
 
             // Extract data
-            uint16_t data_to_copy = cmd_ack.data_length - sizeof(cmd_ack.command);
+            uint16_t data_to_copy = cmd_ack.data_length - sizeof(uint16_t);
             if (data_to_copy > 0)
             {
                 memcpy(cmd_ack.data, &buffer[pos], data_to_copy);
