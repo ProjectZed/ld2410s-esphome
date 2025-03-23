5e605d1e067b6f113427f32ae8665d1375d98727
Kenny Tsui <kjtsui15@gmail.com>
2025-03-23 10:28:53 -0400

    refactor: simplify processByte logic and improve state handling in SensorProcessor

--- components/ld2410s/SensorProcessor.cpp
+++ components/ld2410s/SensorProcessor.cpp
@@ -34,9 +34,67 @@ class SensorProcessor
 public:
     SensorProcessor(const std::unordered_map<uint8_t, HeaderFooter> &headerFooterMap) : headerFooterMap_(headerFooterMap) {}
 
-    std::unique_ptr<Frame> processByte(uint8_t byte);
+    std::unique_ptr<Frame> SensorProcessor::processByte(uint8_t byte)
+    {
+        buffer_.push_back(byte);
+        bufferSize_ += 1;
+        switch (state_)
+        {
+        case ProcessorState::WaitingForHeader:
+        {
+            for (const auto &pair : headerFooterMap_)
+            {
+                const auto &type = pair.first;
+                const auto &headerFooter = pair.second;
+                const auto &header = headerFooter.header;
+                if (buffer_.size() == header.size() && std::equal(header.begin(), header.end(), buffer_.begin()))
+                {
+                    currentType_ = type;
+                    state_ = ProcessorState::ReadingData;
+                    break;
+                }
+            }
+            break;
+        }
+        case ProcessorState::ReadingData:
+        {
+            const auto &footer = headerFooterMap_.at(currentType_).footer;
+            if (byte == footer[0])
+            {
+                footerStartPos_ = bufferSize_ - 1;
+                state_ = ProcessorState::WaitingForFooter;
+            }
+            break;
+        }
+        case ProcessorState::WaitingForFooter:
+        {
+            const auto &footer = headerFooterMap_.at(currentType_).footer;
+            if (bufferSize_ - footerStartPos_ == footer.size() && std::equal(footer.begin(), footer.end(), buffer_.begin() + footerStartPos_))
+            {
+                Frame frame{buffer_, currentType_};
+                buffer_.clear();
+                state_ = ProcessorState::WaitingForHeader;
+                return std::make_unique<Frame>(frame);
+            }
+            else
+            {
+                state_ = ProcessorState::ReadingData;
+            }
+            break;
+        }
+        }
+        return nullptr;
+    }
+    
     ProcessorState getState() const { return state_; }
-    void reset();
+    void reset()
+    {
+        state_ = ProcessorState::WaitingForHeader;
+        buffer_.clear();
+        bufferSize_ = 0;
+        footerStartPos_ = 0;
+        currentType_ = 0;
+    }
 
 private:
     ProcessorState state_ = ProcessorState::WaitingForHeader;
@@ -47,64 +105,3 @@ private:
 
     const std::unordered_map<uint8_t, HeaderFooter> &headerFooterMap_;
 };
-
-std::unique_ptr<Frame> SensorProcessor::processByte(uint8_t byte)
-{
-    buffer_.push_back(byte);
-    bufferSize_ += 1;
-    switch (state_)
-    {
-    case ProcessorState::WaitingForHeader:
-    {
-        for (const auto &pair : headerFooterMap_)
-        {
-            const auto &type = pair.first;
-            const auto &headerFooter = pair.second;
-            const auto &header = headerFooter.header;
-            if (buffer_.size() == header.size() && std::equal(header.begin(), header.end(), buffer_.begin()))
-            {
-                currentType_ = type;
-                state_ = ProcessorState::ReadingData;
-                break;
-            }
-        }
-        break;
-    }
-    case ProcessorState::ReadingData:
-    {
-        const auto &footer = headerFooterMap_.at(currentType_).footer;
-        if (byte == footer[0])
-        {
-            footerStartPos_ = bufferSize_ - 1;
-            state_ = ProcessorState::WaitingForFooter;
-        }
-        break;
-    }
-    case ProcessorState::WaitingForFooter:
-    {
-        const auto &footer = headerFooterMap_.at(currentType_).footer;
-        if (bufferSize_ - footerStartPos_ == footer.size() && std::equal(footer.begin(), footer.end(), buffer_.begin() + footerStartPos_))
-        {
-            Frame frame{buffer_, currentType_};
-            buffer_.clear();
-            state_ = ProcessorState::WaitingForHeader;
-            return std::make_unique<Frame>(frame);
-        }
-        else
-        {
-            state_ = ProcessorState::ReadingData;
-        }
-        break;
-    }
-    }
-    return nullptr;
-}
-
-void SensorProcessor::reset()
-{
-    state_ = ProcessorState::WaitingForHeader;
-    buffer_.clear();
-    bufferSize_ = 0;
-    footerStartPos_ = 0;
-    currentType_ = 0;
-}
