aaa5dcbc821d182894aeb2288392834215a22797
Kenny Tsui <kjtsui15@gmail.com>
2025-04-13 19:04:49 -0400

    fix: enable data format in setup method for LD2410S component

--- components/ld2410s/LD2410S.cpp
+++ components/ld2410s/LD2410S.cpp
@@ -83,7 +83,7 @@ namespace esphome
         void LD2410S::setup()
         {
             this->enable_configuration_command();
-            this->set_data_format(false);
+            this->set_data_format(true);
             this->read_fw_version();
             this->write_common_parameters();
             this->read_common_parameters();
